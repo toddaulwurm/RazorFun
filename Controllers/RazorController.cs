3f957fc8416a61f5c0200065d7240d5b974f0240
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-09-30 14:46:04 -0500

    razor fun?

---
+++ Controllers/RazorController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+namespace RazorFun.Controllers
+{   
+    public class RazorController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
